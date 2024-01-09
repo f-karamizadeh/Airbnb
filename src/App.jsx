50c079d721c15bbf62757b2d6c111afe4b27c011
John Schulte <johannes-schulte@gmx.de>
2024-01-09 18:10:29 +0100

    basic footer implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/header'
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/header";
+import Footer from "./components/footer";
 
 function App() {
-  
-
   return (
     <>
       <Header />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
