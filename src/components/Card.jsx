0af7775b31846cca570aeb56d2367faf72e96401
John Schulte <johannes-schulte@gmx.de>
2024-01-11 19:29:29 +0100

    few fixes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,12 +3,16 @@ import "./Card.css";
 import Slider from "./Slider.jsx";
 import test from "./image4.jpg";
 
-
 export default function Card({ entries }) {
   return (
-    <div class="auto-rows-auto	grid-container">
+    <div className="auto-rows-auto	grid-container">
+      <Slider entries={entries} />
+      <Slider entries={entries} />
+      <Slider entries={entries} />
+      <Slider entries={entries} />
+      <Slider entries={entries} />
+      <Slider entries={entries} />
       <Slider entries={entries} />
     </div>
   );
 }
-
