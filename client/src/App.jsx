4833ae7ee3f318cd91e9285619dc39642887481e
Fara <f_karamizadeh@yahoo.com>
2024-02-01 15:55:44 +0100

    Change word footer in app.js to Footer why Render.com returned it as an Error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
 import Card from "./components/Card";
 import Details from "./components/Details";
 import { createClient } from "contentful";
